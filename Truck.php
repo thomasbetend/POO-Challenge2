c3e79677efa08302abc9cd2983859b255dbed61b
thomasbetend <thomas.betend@yahoo.fr>
2022-10-31 20:47:23 +0100

    challenge accepted

---
+++ Truck.php
@@ -0,0 +1,71 @@
+<?php
+
+require_once('Vehicle.php');
+
+class Truck extends Vehicle {
+    public const ALLOWED_ENERGIES = ['gasoil', 'electric'];
+    private string $energy;
+    private int $energyLevel;
+    private int $loadingCapacity;
+    private int $loading = 0;
+
+public function __construct(string $color, int $nbSeats, string $energy, int $loadingCapacity)
+{
+    parent::__construct($color, $nbSeats);
+    $this->setEnergy($energy);
+    $this->loadingCapacity = $loadingCapacity;
+}
+
+public function fillingTruck() : string {
+    $sentence = "";
+    if($this->loading < 0 || $this->loading > $this->loadingCapacity)  {
+        $sentence = "Loading impossible !";
+    } else if ($this->loading < $this->loadingCapacity){ 
+        $sentence = "In filling.";
+    } else {
+        $sentence = "Full.";
+    }
+    return $sentence;
+}
+
+public function start() : string {
+    $sentence = "Let's go !!!";
+    return $sentence;
+}
+
+public function getEnergy() : string {
+    return $this->energy;
+}
+
+public function setEnergy(string $energy) : Truck {
+    if(in_array($energy, self::ALLOWED_ENERGIES)){
+        $this->energy = $energy;
+    }
+    return $this;
+}
+
+public function getEnergyLevel() : int {
+    return $this->energyLevel;
+}
+
+public function setEnergyLevel(int $energyLevel) : void {
+    $this->energyLevel = $energyLevel;
+}
+
+public function getLoadingCapacity() : int {
+    return $this->energyLevel;
+}
+
+public function setLoadingCapacity(int $loadingCapacity) : void {
+    $this->loadingCapacity = $loadingCapacity;
+}
+
+public function getLoading() : int {
+    return $this->loading;
+}
+
+public function setLoading(int $loading) : void {
+    $this->loading = $loading;
+}
+
+}
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once('Bicycle.php');
+require_once('Car.php');
+require_once('Truck.php');
+
+$bike = new Bicycle('red', 15);
+$car1 = new Car('blue', 3, 'SP95');
+
+$truck1 = new Truck('green', 3, 'gasoil', 40);
+
+$truck1->setLoading(78);
+
+$truck1->setCurrentSpeed(80);
+
+echo $truck1->fillingTruck();
+echo "<br>" . $truck1->start();
+$truck1->setCurrentSpeed(80);
+
+echo "<br>" . $truck1->forward();
+echo "<br>" . $truck1->brake();
+
+
+var_dump($truck1);
+
+
+
+
+
